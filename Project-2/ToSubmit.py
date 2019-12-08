741fe6e3389b159cc2c3e9990d201a851866c107
Isaac Sanchez <jelito@gmail.com>
2019-12-08 21:48:22 +0000

    Project-2 folder created

---
+++ Project-2/ToSubmit.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 30 11:58:04 2019
+
+@author: farshadtoosi
+"""
+
+from pandas import read_csv
+import numpy as np
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from io import StringIO
+from sklearn import linear_model
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+#Note: Not all the libararies above are necessarily needed for this project and not all 
+#the libraries you need for this project are necessarily listed above.
+
+
+""" Your Name and Your student ID: 
+
+
+"""
+
+def task1():
+    
+    
+    
+    
+    
+def task2():
+
+    
+    
+    
+    
+def task3():
+
+    
+    
+    
+    
+def task4():
+
+    
+    
+    
+    
+def task5():
+
+    
+    
+    
+def task6():
+    
+    
+    
+    
+def task7():
+
+    
+    
+    
+    
+    
+    
