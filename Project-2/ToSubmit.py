7064dd01b689985170ff80ddbfef0dde878708b0
Isaac Sanchez <jelito@gmail.com>
2019-12-15 17:12:36 +0000

    Project-2 Ex. 1

--- Project-2/ToSubmit.py
+++ Project-2/ToSubmit.py
@@ -6,79 +6,60 @@ Created on Sat Nov 30 11:58:04 2019
 @author: farshadtoosi
 """
 
-from pandas import read_csv
+from io import StringIO
+
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from pandas import read_csv
 from pandas.plotting import scatter_matrix
-from matplotlib import pyplot
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn import linear_model, preprocessing
+from sklearn.cluster import KMeans
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (accuracy_score, classification_report,
+                             confusion_matrix, mean_squared_error)
+from sklearn.model_selection import (StratifiedKFold, cross_val_score,
+                                     train_test_split)
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-from io import StringIO
-from sklearn import linear_model
-import pandas as pd
-from sklearn.cluster import KMeans
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import mean_squared_error
-import matplotlib.pyplot as plt
 
-#Note: Not all the libararies above are necessarily needed for this project and not all 
-#the libraries you need for this project are necessarily listed above.
+# Note: Not all the libararies above are necessarily needed for this project and not all
+# the libraries you need for this project are necessarily listed above.
 
 
 """ Your Name and Your student ID: 
-
-
+    Isaac Sanchez
+    R00156019
 """
 
+
 def task1():
-    
-    
-    
-    
-    
+    """ Task 1 """
+
+
 def task2():
+    """ Task 2 """
 
-    
-    
-    
-    
+
 def task3():
+    """ Task 3 """
 
-    
-    
-    
-    
+
 def task4():
+    """ Task 4 """
 
-    
-    
-    
-    
+
 def task5():
+    """ Task 5 """
 
-    
-    
-    
+
 def task6():
-    
-    
-    
-    
-def task7():
+    """ Task 6 """
 
-    
-    
-    
-    
-    
-    
+
+def task7():
+    """ Task 7 """
